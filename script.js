74315a9093aca3ada40927746c446c321f49a5df
Ruben Barensen <95089043+RBarensen@users.noreply.github.com>
2022-09-15 08:52:37 +0000

    achtergrond in het spel

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ var massa;
 
 function setup(){
   createCanvas(640, 360);
+  achtergrond = loadImage("images/achtergrond.jpg");
   yval = 0;
   snelheid = 0;
   massa = 50;
@@ -19,7 +20,7 @@ function spel(){
   snelheid += versnelling;
   yval += snelheid;
   image(vogel, width / 2, yval, massa + 20, massa - 10);
-
+  background(achtergrond);
 
   if (yval > height - massa / 2) {
     snelheid *= -0.6;
