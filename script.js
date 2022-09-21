9693522ad097c382b703e2aee30d486e92042416
Ramses Budé <95089348+Rambam111@users.noreply.github.com>
2022-09-21 12:15:19 +0000

    Pijpen als blokjes, vogel nog niet dood.

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
+class rechthoek {
+  constructor(x, y, w, h) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.color = "green";
+  }
+
+  drawrechthoek() {
+    fill(this.color);
+    rect(this.x, this.y, this.w, this.h);
+    this.x += -3;
+  }
+}
+  
+  
 var gif_createImg;
 var yval;
 var versnelling;
 var snelheid;
+var pipes = [];
 var massa;
 
 
@@ -26,11 +44,32 @@ function spel() {
     snelheid *= -0.6;
     yval = height - massa / 2;
   }
+
+  if (frameCount % 85 == 0) {
+
+
+    randomheight = random(height - 150);
+
+    pijp1 = new rechthoek(700, 0, 50, randomheight)
+    pijp2 = new rechthoek(700, randomheight + 150, 50, 300)
+
+    pipes.push(pijp1);
+    pipes.push(pijp2);
+
+    // remove unnessecary pipes
+    if (pipes.length > 6) {
+      pipes.splice(0, 2);
+    }
+  }
+
+    pipes.forEach((p) => {
+    p.drawrechthoek()
+  });
 }
 
 function keyPressed(){
  if (key == ' '){
-  yval + 40;
+  yval + 50;
   snelheid = -massa / 6;
   }
 }
